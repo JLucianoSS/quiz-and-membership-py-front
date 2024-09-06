ce59807485bcd0d449e7ef906f1bd0da5e725824
Jorge <jorgelss912345@gmail.com>
2024-09-06 02:25:26 -0500

    Maquitación básica de página de desempeño

--- src/app/(home)/components/Cards/FavoriteCard.jsx
+++ src/app/(home)/components/Cards/FavoriteCard.jsx
@@ -11,35 +11,41 @@ export const FavoriteCard = ({ pregunta, descripcion, fechaRespuesta, esCorresta
   };
 
   return (
-    <div className="max-w-sm p-4 bg-white rounded-lg border border-gray-100 shadow-md relative">
-      <div className="text-gray-500 text-sm font-semibold">
-        {pregunta}
-      </div>
-      <p className="mt-2 text-gray-600 text-xs overflow-hidden text-ellipsis line-clamp-2">
-        {descripcion}
-      </p>
-      <div className="flex items-center mt-4 text-sm">
-        {esCorresta ? (
-          <IoCheckmarkCircleOutline className="text-green-600 font w-5 h-5 mr-2" />
-        ) : (
-          <IoCloseCircleOutline className="text-red-600 w-5 h-5 mr-2" />
-        )}
-        <span className="text-xs font-semibold text-gray-600">
-          Respondida el {fechaRespuesta}
-        </span>
-      </div>
-      
+    <div className="max-w-sm p-4 bg-white rounded-lg border border-gray-100 shadow-md flex gap-4 items-center">
+
       {/* Botón de favoritos */}
       <button 
         onClick={toggleFavorite}
-        className="absolute top-[5px] right-[5px] focus:outline-none"
+        className="focus:outline-none"
       >
         {isFavorite ? (
           <IoHeart className="text-primary w-6 h-6" /> // Icono sólido (relleno) para cuando es favorito
         ) : (
-          <IoHeartOutline className="text-gray-400 w-6 h-6" /> // Icono outline (no relleno) para cuando no es favorito
+          <IoHeartOutline className="text-primary w-6 h-6" /> // Icono outline (no relleno) para cuando no es favorito
         )}
       </button>
+
+      
+      <div >
+        <div className="text-gray-500 text-sm font-semibold">
+          {pregunta}
+        </div>
+        <p className="mt-2 text-gray-600 text-xs overflow-hidden text-ellipsis line-clamp-2">
+          {descripcion}
+        </p>
+        <div className="flex items-center mt-4 text-sm">
+          {esCorresta ? (
+            <IoCheckmarkCircleOutline className="text-green-600 font w-5 h-5 mr-2" />
+          ) : (
+            <IoCloseCircleOutline className="text-red-600 w-5 h-5 mr-2" />
+          )}
+          <span className="text-xs font-semibold text-gray-600">
+            Respondida el {fechaRespuesta}
+          </span>
+        </div>
+      
+      
+      </div>
     </div>
   );
 };
