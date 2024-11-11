b706272a38f6b5a527caecd6ae860fecea2da443
Jorge <jorgelss912345@gmail.com>
2024-11-11 14:15:34 -0500

    Cerrar sesion en un minuto

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={globalFont.className}>
-        <InactivityProvider timeout={2000}> 
+        <InactivityProvider timeout={18000000}> 
           {children}
           <Toaster />
         </InactivityProvider>
